6cfe904d6455ed5c2da1d11f8baec7b38c339d20
Victor Arruda Magalhaes <victor@ammora.email>
2022-04-08 00:14:34 -0300

    feat: configura eslint

--- src/pages/destinos_nacionais.js
+++ src/pages/destinos_nacionais.js
@@ -1,18 +1,19 @@
-import { Card, Footer, Header, Hero } from '../components/Structure';
+import {
+  Card, Footer, Header, Hero,
+} from '../components/Structure';
 import { Container } from '../components/Foundation';
 import client from '../utils/client';
 import { getAllNationalDestinations } from '../utils/queries';
 
 export default function NationalDestinations({ destinations }) {
-
   return (
     <>
       <Header />
       <div>
-        <Hero search={false} title='Destinos Nacionais' uri='/bg-national-destinations.jpg' />
-        <Container newClasses='py-16'>
-          <div className='lg:col-span-10 grid lg:grid-cols-9 gap-x-8 gap-y-16'>
-            {destinations?.map(destination => (
+        <Hero search={false} title="Destinos Nacionais" uri="/bg-national-destinations.jpg" />
+        <Container newClasses="py-16">
+          <div className="lg:col-span-10 grid lg:grid-cols-9 gap-x-8 gap-y-16">
+            {destinations?.map((destination) => (
               <Card
                 date={destination.date}
                 key={destination.id}
@@ -24,27 +25,27 @@ export default function NationalDestinations({ destinations }) {
               />
             ))}
           </div>
-          <div className='lg:col-span-2'>
+          <div className="lg:col-span-2">
             <p>Publicidade</p>
           </div>
         </Container>
       </div>
       <Footer />
     </>
-  )
+  );
 }
 
 export async function getStaticProps(context) {
   const { data } = await client.query({
-    query: getAllNationalDestinations
+    query: getAllNationalDestinations,
   });
 
   const destinations = data?.destinosNacionais?.edges?.map(({ node }) => node);
 
   return {
     props: {
-      destinations
+      destinations,
     },
-    revalidate: 600
-  }
+    revalidate: 600,
+  };
 }
