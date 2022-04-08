8331775a6786fe94eb3c8b175873b820d90afb80
Victor Arruda Magalhaes <victor@ammora.email>
2022-04-08 01:17:05 -0300

    feat: ajusta lint de alguns arquivos

--- src/pages/api/mail.js
+++ src/pages/api/mail.js
@@ -2,7 +2,7 @@ const mail = require('nodemailer');
 
 export default function sendMail(req, res) {
   const body = JSON.parse(req.body);
-  
+
   const message = `
     Nome: ${body.nome}\r\n
     E-mail: ${body.email}\r\n
@@ -10,22 +10,27 @@ export default function sendMail(req, res) {
     Mensagem: ${body.mensagem}
   `;
 
-  let transporter = mail.createTransport({
+  const transporter = mail.createTransport({
     host: 'smtp.gmail.com',
     port: '587',
     auth: {
       user: process.env.NODEMAILER_USER,
-      pass: process.env.NODEMAILER_PASS
-    }
+      pass: process.env.NODEMAILER_PASS,
+    },
   });
 
-  transporter.sendMail({
-    from: body.email,
-    to: `${process.env.NODEMAILER_USER}`,
-    subject: 'Contato via site',
-    text: body.message,
-    html: message.replace(/\r\n/g, '<br />')
-  })
-  .then(response => { res.send(response) })
-  .catch(error => { res.send(error) })
+  transporter
+    .sendMail({
+      from: body.email,
+      to: `${process.env.NODEMAILER_USER}`,
+      subject: 'Contato via site',
+      text: body.message,
+      html: message.replace(/\r\n/g, '<br />'),
+    })
+    .then((response) => {
+      res.send(response);
+    })
+    .catch((error) => {
+      res.send(error);
+    });
 }
--- src/pages/destino_nacional/[slug].js
+++ src/pages/destino_nacional/[slug].js
@@ -1,84 +1,126 @@
-import { Footer, Header, Hero } from '../../components/Structure';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFacebook, faLinkedinIn, faTwitter, faWhatsapp } from '@fortawesome/fontawesome-free-brands';
+import {
+  faFacebook,
+  faLinkedinIn,
+  faTwitter,
+  faWhatsapp,
+} from '@fortawesome/fontawesome-free-brands';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { Footer, Header, Hero } from '../../components/Structure';
 import client from '../../utils/client';
-import { getAllNationalDestinationsSlugs, getNationalDestinationBySlug } from '../../utils/queries';
+import {
+  getAllNationalDestinationsSlugs,
+  getNationalDestinationBySlug,
+} from '../../utils/queries';
 import { Container } from '../../components/Foundation';
 
 export default function DestinationPage({ destination }) {
   return (
     <>
       <Header />
-      <Hero search={false} title={destination.title} uri={destination.featuredImage.node.sourceUrl} />
+      <Hero
+        search={false}
+        title={destination.title}
+        uri={destination.featuredImage.node.sourceUrl}
+      />
       <Container>
-        <div className='col-span-6 col-start-4 grid grid-cols-6 gap-6'>
-          <h2 className='col-span-4 font-serif text-custom-gray text-xl'>{destination.title}</h2>
-          <ul className='col-span-2 flex justify-end space-x-3'>
+        <div className="col-span-6 col-start-4 grid grid-cols-6 gap-6">
+          <h2 className="col-span-4 font-serif text-custom-gray text-xl">
+            {destination.title}
+          </h2>
+          <ul className="col-span-2 flex justify-end space-x-3">
             <li>
-              <a href='#this' rel='noreferrer' target='_blank' title='Facebook'>
-                <FontAwesomeIcon className='h-5 text-soft-gray w-5' icon={faFacebook} />
+              <a href="#this" rel="noreferrer" target="_blank" title="Facebook">
+                <FontAwesomeIcon
+                  className="h-5 text-soft-gray w-5"
+                  icon={faFacebook}
+                />
               </a>
             </li>
             <li>
-              <a href='#this' rel='noreferrer' target='_blank' title='Twitter'>
-                <FontAwesomeIcon className='h-5 text-soft-gray w-5' icon={faTwitter} />
+              <a href="#this" rel="noreferrer" target="_blank" title="Twitter">
+                <FontAwesomeIcon
+                  className="h-5 text-soft-gray w-5"
+                  icon={faTwitter}
+                />
               </a>
             </li>
             <li>
-              <a href='#this' rel='noreferrer' target='_blank' title='LinkedinIn'>
-                <FontAwesomeIcon className='h-5 text-soft-gray w-5' icon={faLinkedinIn} />
+              <a
+                href="#this"
+                rel="noreferrer"
+                target="_blank"
+                title="LinkedinIn"
+              >
+                <FontAwesomeIcon
+                  className="h-5 text-soft-gray w-5"
+                  icon={faLinkedinIn}
+                />
               </a>
             </li>
             <li>
-              <a href='#this' rel='noreferrer' target='_blank' title='WhatsApp'>
-                <FontAwesomeIcon className='h-5 text-soft-gray w-5' icon={faWhatsapp} />
+              <a href="#this" rel="noreferrer" target="_blank" title="WhatsApp">
+                <FontAwesomeIcon
+                  className="h-5 text-soft-gray w-5"
+                  icon={faWhatsapp}
+                />
               </a>
             </li>
             <li>
-              <a href='#this' rel='noreferrer' target='_blank' title='E-mail'>
-                <FontAwesomeIcon className='h-5 text-soft-gray w-5' icon={faEnvelope} />
+              <a href="#this" rel="noreferrer" target="_blank" title="E-mail">
+                <FontAwesomeIcon
+                  className="h-5 text-soft-gray w-5"
+                  icon={faEnvelope}
+                />
               </a>
             </li>
           </ul>
-          <h1 className='col-span-6 text-4xl text-custom-purple'>{destination.custom_destinations_fields.summary}</h1>
-          <p className='col-span-6 font-serif text-custom-gray text-xs'>Publicado em: {new Date(destination.date).toLocaleDateString('pt-BR')}</p>
-          <div className='col-span-6 teste' dangerouslySetInnerHTML={{ __html: destination.custom_destinations_fields.content }} />
+          <h1 className="col-span-6 text-4xl text-custom-purple">
+            {destination.custom_destinations_fields.summary}
+          </h1>
+          <p className="col-span-6 font-serif text-custom-gray text-xs">
+            Publicado em:{' '}
+            {new Date(destination.date).toLocaleDateString('pt-BR')}
+          </p>
+          <div
+            className="col-span-6 teste"
+            dangerouslySetInnerHTML={{
+              __html: destination.custom_destinations_fields.content,
+            }}
+          />
         </div>
       </Container>
       <Footer />
     </>
-  )
+  );
 }
 
 export async function getStaticPaths() {
   const { data } = await client.query({
-    query: getAllNationalDestinationsSlugs
+    query: getAllNationalDestinationsSlugs,
   });
 
   return {
-    paths: data?.destinosNacionais?.nodes?.map(({ slug }) => {
-      return {
-        params: { slug }
-      };
-    }),
-    fallback: false
+    paths: data?.destinosNacionais?.nodes?.map(({ slug }) => ({
+      params: { slug },
+    })),
+    fallback: false,
   };
-};
+}
 
 export async function getStaticProps({ params }) {
   const { slug } = params;
   const { data } = await client.query({
     query: getNationalDestinationBySlug,
-    variables: { slug }
+    variables: { slug },
   });
 
   const destination = data?.destinoNacionalBy;
 
   return {
     props: {
-      destination
+      destination,
     },
-    revalidate: 600
-  }
+    revalidate: 600,
+  };
 }
